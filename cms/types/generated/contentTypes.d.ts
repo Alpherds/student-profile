23dfc95a595a3506afae321672cf46cc6db4d3d4
Alpher <alphers.dizon@gmail.com>
2026-06-01 08:26:42 +0800

    test button

--- cms/types/generated/contentTypes.d.ts
+++ cms/types/generated/contentTypes.d.ts
@@ -477,6 +477,34 @@ export interface ApiStudInfoStudInfo extends Struct.CollectionTypeSchema {
   };
 }
 
+export interface ApiTestinfoTestinfo extends Struct.CollectionTypeSchema {
+  collectionName: 'testinfos';
+  info: {
+    displayName: 'testinfo';
+    pluralName: 'testinfos';
+    singularName: 'testinfo';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    Fname: Schema.Attribute.String;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::testinfo.testinfo'
+    > &
+      Schema.Attribute.Private;
+    publishedAt: Schema.Attribute.DateTime;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface PluginContentReleasesRelease
   extends Struct.CollectionTypeSchema {
   collectionName: 'strapi_releases';
@@ -989,6 +1017,7 @@ declare module '@strapi/strapi' {
       'admin::transfer-token-permission': AdminTransferTokenPermission;
       'admin::user': AdminUser;
       'api::stud-info.stud-info': ApiStudInfoStudInfo;
+      'api::testinfo.testinfo': ApiTestinfoTestinfo;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
       'plugin::i18n.locale': PluginI18NLocale;
--- web/app/pages/index.vue
+++ web/app/pages/index.vue
@@ -11,19 +11,52 @@
                         Setup is working: {{ config.public.strapiUrl }}
                     </v-card-text>
 
-                    <v-btn>
+                    <v-btn @click="loadStudinfo">
                         Test button
                     </v-btn>
+
+                    <v-alert v-if="loading" type="info">
+                        loading data...
+                    </v-alert>
+
+                    <v-alert v-if="errorMessage" type="error">
+                        {{ errorMessage }}
+                    </v-alert>
+                </v-card>
+
+                <v-card
+                    v-for="student in studinfos"
+                    :key="student.id">
+                    <pre>{{ student }}</pre>
                 </v-card>
             </v-container>
         </v-main>
     </v-app>
 </template>
 
-<script setup>
-    const config= useRuntimeConfig()
+<script setup lang="ts">
+   const config = useRuntimeConfig()
 
-    const {data , pending, error } = await useFetch(
-        '${config.public.strapiUrl}/api'
-    )
+   const studinfos = ref<any[]>([])
+
+   const loading = ref(false)
+
+   const errorMessage = ref('')
+   
+
+   const loadStudinfo = async () => {
+        loading.value = true
+        errorMessage.value = ''
+
+        try {
+            const response: any = await $fetch(
+                `${config.public.strapiUrl}/api/stud-infos`
+            )
+            studinfos.value = response.data
+        } catch (error: any) {
+            errorMessage.value = 'Failed to load studinfos: ' + error.message
+        } finally {
+            loading.value = false
+        }
+   }
 </script>
